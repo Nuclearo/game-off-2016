1667ee2705c219b6577bec5c28aae5463e60ecd4
Nuclearo <nuclearfury8@gmail.com>
2016-11-12 12:20:41 +0200

    Initial GDX project generation

---
+++ desktop/src/nukey/nova/cool/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nukey.nova.cool.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nukey.nova.cool.Cool;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Cool(), config);
+	}
+}
