4ee080dd2a05f910e1617c23f6a4ce34a00aa941
Nuclearo <nuclearfury8@gmail.com>
2016-11-16 07:06:08 +0200

    add camera control

---
+++ core/src/nukey/nova/cool/CameraInputProcessor.java
@@ -0,0 +1,69 @@
+package nukey.nova.cool;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Camera;
+
+public class CameraInputProcessor implements InputProcessor {
+	private Camera _cam;
+	
+	public CameraInputProcessor(Camera cam){
+		this._cam = cam;
+	}
+	
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		if(Gdx.input.isButtonPressed(Buttons.RIGHT)){
+			int deltaX = -Gdx.input.getDeltaX();
+			int deltaY = Gdx.input.getDeltaY();
+			_cam.translate(deltaX, deltaY, 0);
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- core/src/nukey/nova/cool/Cool.java
+++ core/src/nukey/nova/cool/Cool.java
@@ -3,23 +3,40 @@ package nukey.nova.cool;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 
 public class Cool extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
+	private OrthographicCamera cam;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+		cam = new OrthographicCamera(w,h);
+        cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 2);
+        cam.update();
+        
+        Gdx.input.setInputProcessor(new CameraInputProcessor(cam));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+        cam.update();
+		batch.setProjectionMatrix(cam.combined);
+		
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
